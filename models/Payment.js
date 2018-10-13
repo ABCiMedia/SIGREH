a67b4d989d70f1e41deeb7421af8eba74347b0a2
Ulisses Cruz <ulissescruz10@gmail.com>
2018-10-13 12:54:59 -0100

    Criou o modelo Payment

---
+++ models/Payment.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+const cred = require('./credentials')
+    , {Person} = require('./Person');
+
+const connection = new Sequelize(cred.database, cred.username, cred.password, {
+    host: cred.host,
+    dialect: cred.dialect,
+    operatorsAliases: false,
+    logging: false
+});
+
+const Payment = connection.define('payment', {
+    toPay: Sequelize.REAL,
+    paid: Sequelize.REAL,
+    discount: Sequelize.REAL
+});
+
+Payment.belongsTo(Person);
+
+Payment.sync();
+
+module.exports = {
+    Payment
+}
