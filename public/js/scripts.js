6504e8c52d9c92bb61901b6e18bd81577c2fa53a
Ulisses Cruz <ulissescruz10@gmail.com>
2018-10-22 17:04:26 -0100

    Modified the register form for Materialize Added datepicker

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -111,3 +111,62 @@ const openSidenav = () => {
     let inst = M.Sidenav.init(el)[0];
     inst.open();
 };
+
+const showDatepicker = () => {
+    let el = document.querySelectorAll('.datepicker');
+    let inst = M.Datepicker.init(el, {
+        format: 'dd-mm-yyyy',
+        i18n: {
+            cancel: 'Cancelar',
+            clear: 'Limpar',
+            months: [
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro'
+            ],
+            monthsShort: [
+                'Jan',
+                'Fev',
+                'Mar',
+                'Abr',
+                'Mai',
+                'Jun',
+                'Jul',
+                'Ago',
+                'Set',
+                'Out',
+                'Nov',
+                'Dez'
+            ],
+            weekdays: [
+                'Domingo',
+                'Segunda-Feira',
+                'Terça-Feira',
+                'Quarta-Feira',
+                'Quinta-Feira',
+                'Sexta-Feira',
+                'Sábado'
+            ],
+            weekdaysShort: [
+                'Dom',
+                'Seg',
+                'Ter',
+                'Qua',
+                'Qui',
+                'Sex',
+                'Sab'
+            ],
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+        }
+    })[0];
+    inst.open();
+}
